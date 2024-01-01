4db2750db7a0b8609c61bb00d4e917041345b1cd
Ashfaque-A <mashfaque624@gmail.com>
2024-01-01 16:56:10 +0530

    textbook

--- dailay task/HTML CSS JS/self_practice.js
+++ dailay task/HTML CSS JS/self_practice.js
@@ -1,10 +1,36 @@
 
-function saddam(name,age,subject){
 
+function saddam(){
 
+    var fname = document.getElementById('fname').value
+    var lname = document.getElementById('lname').value
+    var email = document.getElementById('email').value
+    var add = document.getElementById('add').value
+    var sub = document.getElementById('sub').value
 
+  var result = (fname , lname , email , add ,sub);
 
+   if(result==0){
+    // document.write("PLEASE INSERT YOUR DATA");
+    alert("PLEASE INSERT YOUR DATA")
+   }
+  
+else if(fname=="ashfaque" && lname=="alam" && email=="mashfaque624@gmail.com" && add=="patna" && sub=="math"){
+    // document.getElementById("demo").innerHTML="WELCOME TO MY ACCOUTNT";
+    location.replace("page.html")
 }
-saddam("saddam",21,"math");
+else{
+document.getElementById("demo").innerHTML="YOUR ACCOUNT NOT CONNECT RETRY AGAIN";
+};
 
+
+// var result= document.write(fname , lname , email , add ,sub).value;
+   
+};
+
+ 
+
+
+
+ 
 
